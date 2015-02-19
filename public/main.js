7d0eed28258f1e1450bab8a197bb86fa176a0a3a
neelp22 <neelp22@gmail.com>
2015-02-19 14:25:56 -0500

    updated to reference firebase .ref())

--- public/main.js
+++ public/main.js
@@ -13,7 +13,7 @@ var roomName = "WDI Zelda Room"
 
 roomRef.once("value", function(snapshot){
 	snapshot.forEach(function(data){
-		if(data.name() == roomName){
+		if(data.key() == roomName){
 			currentRoom = data
 			loadChat();
 		}else{
@@ -29,12 +29,11 @@ function loadChat(){
 	//here we create the chat 'history' for the room. Above, we created myroom. Now we append a child to that object, and call it the history
 	//eventually we will push new messages into that chathistory object
 
-	//FIXME we need to attach the listener to a firebase snapshot, not a plain old object
 	chatHistory = currentRoom.child("chatHistory");
 
 	//call.key on an object to get its unique id. You can see this id in the GUI
 	//can use this later to check if a user is auth'd to receive messages
-	myRoomID = currentRoom.key()
+	myRoomName = currentRoom.key()
 
 	addFirebaseListeners()
 };
@@ -53,20 +52,21 @@ function addFirebaseListeners(){
 	//function to append messages to the DOM. The function takes a snapshot as an argument. The snapshot is the
 	//firebase reponse or object. If you 'push' a new object into a reference, the most recent object will be returned
 	//as the snapshot
-	function appendMessage(snapshot){
+function appendMessage(snapshot){
 	var message = snapshot.val();
 	var newLi = $("<li>");
 	newLi.text(message.name + " says: " + message.text)
 	$("#messagesList").append(newLi);
-	};
+};
 
-	//a bit of jquery to grab input and make a call to your firebase when 'return' is hit
-	$('#messageInput').keypress(function (e) {
-	if (e.keyCode == 13) {
-	  var name = $('#nameInput').val();
-	  var text = $('#messageInput').val();
-		chatHistory.push({name: name, text: text});
-	  $('#messageInput').val('');
-	  $('#nameInput').val('');
-	}
-	});
+//a bit of jquery to grab input and make a call to your firebase when 'return' is hit
+$('#messageInput').keypress(function (e) {
+if (e.keyCode == 13) {
+  var name = $('#nameInput').val();
+  var text = $('#messageInput').val();
+  debugger
+	chatHistory.ref().push({name: name, text: text});
+  $('#messageInput').val('');
+  $('#nameInput').val('');
+}
+});
